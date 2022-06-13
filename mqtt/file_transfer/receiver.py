216107093c2328bbf9df6b95d9ac1177166fc9cc
Pranav Kumar <pranavkr@iitk.ac.in>
2022-06-13 08:59:10 +0530

    improve

--- mqtt/file_transfer/receiver.py
+++ mqtt/file_transfer/receiver.py
@@ -2,16 +2,16 @@ import paho.mqtt.client as mqtt
 import time
 
 
-def on_message(mosq, obj, msg):                             # Function to retrieve file when received
-  with open('imagenow.jpeg', 'wb') as fd:
+def on_message(mosq, obj, msg):                            
+  with open('receive_img.jpeg', 'wb') as fd:
       fd.write(msg.payload)
 
 
-client = mqtt.Client("S1")                                # Start MQTT Client, NB changed to P2
-client.connect("localhost", 1883, 60)                 # Connect to server
+client = mqtt.Client("S1")                               
+client.connect("localhost", 1883, 60)               
 client.subscribe("highmount/camera",0)
 
-client.on_message = on_message                            # This is key - it calls the function
+client.on_message = on_message                          
 
-while True:                                               # Loop and wait for next image
+while True:                                             
    client.loop(20)
--- mqtt/file_transfer/sender.py
+++ mqtt/file_transfer/sender.py
@@ -2,14 +2,14 @@ import paho.mqtt.client as mqtt
 import time
 
 broker="localhost"
-client = mqtt.Client("P1")                                # Start MQTT Client
-client.connect(broker, 1883, 60)                 # Connect to server
+client = mqtt.Client("P1")                                
+client.connect(broker, 1883, 60)                 
 
-client.loop_start()                                       # initial start before loop
+client.loop_start()                                      
 
 while True:
-    file = open("flower.jpeg", "rb")         # open the file, note r = read, b = binary
-    imagestring = file.read()                                            # read the file
-    byteArray = bytes(imagestring)                                       # convert to byte string
-    client.publish(topic="highmount/camera", payload= byteArray ,qos=0)  # publish it to the MQ queue
-    time.sleep(20)                                                       # wait for next image
+    file = open("flower.jpeg", "rb")         
+    imagestring = file.read()                                          
+    byteArray = bytes(imagestring)                                     
+    client.publish(topic="highmount/camera", payload= byteArray ,qos=0) 
+    time.sleep(20)                                                       
